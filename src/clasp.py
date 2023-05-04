6291bf438310f63d3eb58a9d9a7f3b764ffd654d
Wasim <wasimahmad.qc@gmail.com>
2023-05-04 17:43:40 +0200

    Time series segmentation with ClaSP

--- src/clasp.py
+++ src/clasp.py
@@ -1,3 +1,6 @@
+from claspy.segmentation import BinaryClaSPSegmentation
+from claspy.data_loader import load_tssb_dataset
+
 def ClaSP(time_series, tcps):
     
     plt.plot(time_series)
@@ -7,3 +10,7 @@ def ClaSP(time_series, tcps):
     clasp.plot(gt_cps=tcps, heading="Segmentation", ts_name="ACC", file_path="ts_segmentation.png")
     
     return regimes
+
+dataset, window_size, true_cps, time_series = load_tssb_dataset(names=("CricketX", )).iloc[0, :]
+# clusters = ClaSP(time_series, true_cps)
+# print(clusters)
